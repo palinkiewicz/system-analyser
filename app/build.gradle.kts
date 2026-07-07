8db80552e0fd548f27e83b8dc133862db05d72d3
palinkiewicz <pogromca.ap@gmail.com>
2026-07-07 08:16:30 +0200

    Change app's version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "pl.dakil.appanalyser"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
